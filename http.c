valdisxp1/quark-webserver

Rename REQ_MOD to REQ_IF_MODIFIED_SINCE
The named constants for header fields of the response struct all
pretty much matched the actual header name, which I think improves
readability for everyone familiar with the HTTP-spec.

The request header fields named constants followed the rule, except
the "If-Modified-Since"-header, which is addressed in this commit.

Signed-off-by: Laslo Hunhold <dev@frign.de>

http.c
@@ -22,9 +22,9 @@
 #include "util.h"
 
 const char *req_field_str[] = {
-	[REQ_HOST]    = "Host",
-	[REQ_RANGE]   = "Range",
-	[REQ_MOD]     = "If-Modified-Since",
+	[REQ_HOST]              = "Host",
+	[REQ_RANGE]             = "Range",
+	[REQ_IF_MODIFIED_SINCE] = "If-Modified-Since",
 };
 
 const char *req_method_str[] = {
@@ -671,9 +671,10 @@ http_send_response(int fd, struct request *req)
 	}
 
 	/* modified since */
-	if (req->field[REQ_MOD][0]) {
+	if (req->field[REQ_IF_MODIFIED_SINCE][0]) {
 		/* parse field */
-		if (!strptime(req->field[REQ_MOD], "%a, %d %b %Y %T GMT", &tm)) {
+		if (!strptime(req->field[REQ_IF_MODIFIED_SINCE],
+		              "%a, %d %b %Y %T GMT", &tm)) {
 			return http_send_status(fd, S_BAD_REQUEST);
 		}
 

jeremybobbin/quark

http.c
@@ -22,9 +22,9 @@
 #include "util.h"
 
 const char *req_field_str[] = {
-	[REQ_HOST]    = "Host",
-	[REQ_RANGE]   = "Range",
-	[REQ_MOD]     = "If-Modified-Since",
+	[REQ_HOST]              = "Host",
+	[REQ_RANGE]             = "Range",
+	[REQ_IF_MODIFIED_SINCE] = "If-Modified-Since",
 };
 
 const char *req_method_str[] = {
@@ -524,9 +524,10 @@ http_send_response(int fd, struct request *r)
 	}
 
 	/* modified since */
-	if (r->field[REQ_MOD][0]) {
+	if (r->field[REQ_IF_MODIFIED_SINCE][0]) {
 		/* parse field */
-		if (!strptime(r->field[REQ_MOD], "%a, %d %b %Y %T GMT", &tm)) {
+		if (!strptime(r->field[REQ_IF_MODIFIED_SINCE],
+		              "%a, %d %b %Y %T GMT", &tm)) {
 			return http_send_status(fd, S_BAD_REQUEST);
 		}
 